7e38f5d359df735f82c2dd67ef007274dc1734dd
monalipatane <monalipatane05@gmail.com>
2025-09-26 11:11:54 +0530

    Add files via upload

---
+++ OneDArray.java
@@ -0,0 +1,11 @@
+public class OneDArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
+
